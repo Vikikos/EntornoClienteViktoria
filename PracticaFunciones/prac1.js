a69345643090bb33fc4a74cbc7e80298356c2fab
Vikikos <viktoriakosarova2722gmail.com>
2025-10-20 20:19:53 +0200

    v14

--- PracticaFunciones/prac1.js
+++ PracticaFunciones/prac1.js
@@ -1,9 +1,8 @@
 let palabras =  pedirpalabra();
 
-palabras.sort();//ordenamos alfabeticamente
-
-
-palabras.reverse();
+palabras = new Set(palabras);//borra los repetidos trasnformando en set
+const arrayPalabras = [...palabras];//lo convertimos en array de nuevo para usar las funciones de Array
+arrayPalabras.sort().reverse();//ordenamos alfabeticamente en reverse
 
 let caja = document.getElementById('res');
-caja.textContent = palabras;
+caja.textContent = arrayPalabras;
